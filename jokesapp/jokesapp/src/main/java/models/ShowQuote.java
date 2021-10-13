ff9e817285b825763f7182e0c045f46bf67ea4ce
GeorgeXenidis <33318467+GeorgeXenidis@users.noreply.github.com>
2021-10-13 16:08:28 +0300

    Update ShowQuote.java

--- jokesapp/jokesapp/src/main/java/models/ShowQuote.java
+++ jokesapp/jokesapp/src/main/java/models/ShowQuote.java
@@ -5,9 +5,10 @@ import guru.springframework.norris.chuck.ChuckNorrisQuotes;
 
 public class ShowQuote implements PrintQuote {
 
+    //This instance is created inside this static context so it can
+    // access methods inside ChuckNorrisQuotes non-static Class.
     ChuckNorrisQuotes quote;
     public ShowQuote() {
-        //This instance is created inside this static context so it can access methods inside ChuckNorrisQuotes non-static Class.
         this.quote = new ChuckNorrisQuotes();
     }
 
