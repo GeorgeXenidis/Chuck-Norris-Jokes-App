dad4c9dbe69143ef8f99dd73ae38836bf396c14b
GeorgeXenidis <33318467+GeorgeXenidis@users.noreply.github.com>
2021-10-13 18:19:00 +0300

    Corrected bugs

--- jokesapp/jokesapp/src/main/java/me/springboot/jokesapp/JokesappApplication.java
+++ jokesapp/jokesapp/src/main/java/me/springboot/jokesapp/JokesappApplication.java
@@ -2,6 +2,7 @@ package me.springboot.jokesapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class JokesappApplication {
@@ -9,5 +10,6 @@ public class JokesappApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(JokesappApplication.class, args);
 	}
+
 
 }
