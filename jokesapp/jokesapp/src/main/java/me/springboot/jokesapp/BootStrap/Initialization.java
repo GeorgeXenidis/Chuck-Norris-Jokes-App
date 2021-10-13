d17949664b9dab70410eba52e477d5725060e3c2
GeorgeXenidis <33318467+GeorgeXenidis@users.noreply.github.com>
2021-10-13 18:19:22 +0300

    Added an initialization method for the app

---
+++ jokesapp/jokesapp/src/main/java/me/springboot/jokesapp/BootStrap/Initialization.java
@@ -0,0 +1,13 @@
+package me.springboot.jokesapp.BootStrap;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Initialization implements CommandLineRunner {
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
